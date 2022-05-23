7373c347594990040b02ea245e3442a40433b17d
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-05-23 22:09:56 +0500

    using reducers

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,11 +1,13 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useReducer } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
 	const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
 
 	if (existingCartItem) {
 		return cartItems.map((cartItem) =>
-			cartItem.id === productToAdd.id ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
+			cartItem.id === productToAdd.id
+				? { ...cartItem, quantity: cartItem.quantity + 1 }
+				: cartItem
 		);
 	}
 
@@ -13,14 +15,18 @@ const addCartItem = (cartItems, productToAdd) => {
 };
 
 const removeCartItem = (cartItems, cartItemToRemove) => {
-	const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === cartItemToRemove.id
+	);
 
 	if (existingCartItem.quantity === 1) {
 		return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
 	}
 
 	return cartItems.map((cartItem) =>
-		cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem
+		cartItem.id === cartItemToRemove.id
+			? { ...cartItem, quantity: cartItem.quantity - 1 }
+			: cartItem
 	);
 };
 
@@ -38,6 +44,26 @@ export const CartContext = createContext({
 	cartTotal: 0,
 });
 
+const INITIAL_STATE = {
+	isCartOpen: false,
+	cartItems: [],
+	cartCount: 0,
+	cartTotal: 0,
+};
+
+const cartReducer = (state, action) => {
+	const { type, payload } = action;
+
+	switch (type) {
+		case 'ADD_TO_CART':
+			return {
+				...state,
+			};
+		default:
+			throw new Error(`Unhandled type of ${type} in cartReducer`);
+	}
+};
+
 export const CartProvider = ({ children }) => {
 	const [isCartOpen, setIsCartOpen] = useState(false);
 	const [cartItems, setCartItems] = useState([]);
@@ -45,7 +71,10 @@ export const CartProvider = ({ children }) => {
 	const [cartTotal, setCartTotal] = useState(0);
 
 	useEffect(() => {
-		const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+		const newCartCount = cartItems.reduce(
+			(total, cartItem) => total + cartItem.quantity,
+			0
+		);
 		setCartCount(newCartCount);
 	}, [cartItems]);
 
